d675ed8fba5f8661abe6f781c678845db324f6ca
amaliarowena <amalia_hertel@dell.com>
2016-08-23 15:09:29 -0500

    Renaming Files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class storagemanager (
+class dellstorageprovisioning (
   # Parameters required for login
   $ip_address, # DNS Host Name or IP Address of the API server
   $username, # DSM User to log in as
@@ -21,7 +21,7 @@ class storagemanager (
   $tear_down = false,
   $default_storage_center          = 66090,) {
   # login to the dsm
-  storagemanager_login { "$ip_address":
+  dellstorageprovisioning_login { "$ip_address":
     username => "$username",
     password => "$password",
     ensure   => present,
@@ -29,31 +29,31 @@ class storagemanager (
 
   if $tear_down == true {
     # Deletion must be done from child up to parent
-    class { 'storagemanager::volume':
+    class { 'dellstorageprovisioning::volume':
       volume_definition_array => $volume_definition_array,
       tear_down               => $tear_down,
       storage_center          => $default_storage_center,
     }
 
-    class { 'storagemanager::volume_folder':
+    class { 'dellstorageprovisioning::volume_folder':
       folder_definition_array => $volume_folder_definition_array,
       tear_down               => $tear_down,
       storage_center          => $default_storage_center,
     }
 
-    class { 'storagemanager::server':
+    class { 'dellstorageprovisioning::server':
       server_definition_array => $server_definition_array,
       tear_down               => $tear_down,
       storage_center          => $default_storage_center,
     }
 
-    class { 'storagemanager::server_cluster':
+    class { 'dellstorageprovisioning::server_cluster':
       server_cluster_definition_array => $server_cluster_definition_array,
       tear_down      => $tear_down,
       storage_center => $default_storage_center,
     }
 
-    class { 'storagemanager::server_folder':
+    class { 'dellstorageprovisioning::server_folder':
       folder_definition_array => $server_folder_definition_array,
       tear_down               => $tear_down,
       storage_center          => $default_storage_center,
@@ -61,39 +61,39 @@ class storagemanager (
 
   } else {
     # Creation must be done from parent down to child
-    class { 'storagemanager::server_folder':
+    class { 'dellstorageprovisioning::server_folder':
       folder_definition_array => $server_folder_definition_array,
       storage_center          => $default_storage_center,
     }
 
-    class { 'storagemanager::server_cluster':
+    class { 'dellstorageprovisioning::server_cluster':
       server_cluster_definition_array => $server_cluster_definition_array,
       storage_center                  => $default_storage_center,
     }
 
-    class { 'storagemanager::server':
+    class { 'dellstorageprovisioning::server':
       server_definition_array => $server_definition_array,
       storage_center          => $default_storage_center,
     }
 
     unless $hba_definition_array == [] {
-      class { 'storagemanager::hba':
+      class { 'dellstorageprovisioning::hba':
         hba_definition_array => $hba_definition_array,
         storage_center       => $default_storage_center,
       }
     }
 
-    class { 'storagemanager::volume_folder':
+    class { 'dellstorageprovisioning::volume_folder':
       folder_definition_array => $volume_folder_definition_array,
       storage_center          => $default_storage_center,
     }
 
-    class { 'storagemanager::volume':
+    class { 'dellstorageprovisioning::volume':
       volume_definition_array => $volume_definition_array,
       storage_center          => $default_storage_center,
     }
 
-    class { 'storagemanager::mapping':
+    class { 'dellstorageprovisioning::mapping':
       mapping_definition_array => $mapping_definition_array,
       storage_center           => $default_storage_center,
     }
