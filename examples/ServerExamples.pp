28ffed52863e49f496600b0220d78cade30f62a0
amaliarowena <amalia_hertel@dell.com>
2016-08-24 17:04:14 -0500

    Add example/tests

---
+++ examples/ServerExamples.pp
@@ -0,0 +1,96 @@
+# This is an example manifest showing how to create Servers, Server Clusters, and Server Folders, and add HBAs.
+# Remember to fill in your HBA information in addition to the information at the top of the file.
+class { 'dellstorageprovisioning':
+  ip_address => "YOUR DSM IP ADDRESS",
+  password   => "YOUR PASSWORD",
+  username   => "YOUR USERNAME",
+  tear_down  => false, # Change to 'true' to remove set-up
+  default_storage_center          => "YOUR SC ID",
+  server_folder_definition_array  => [
+    {
+      num_folders   => 1,
+      do_not_number => true,
+      folder_name   => "SingleFolder",
+    }
+    ,
+    {
+      num_folders => 3,
+      folder_name => "SeriesFolder",
+      notes       => "Example Notes",
+      parent_name => "SingleFolder",
+    }
+    ,
+    {
+      folder_name    => [
+        "1-ArrayFolder",
+        "2-ArrayFolder",
+        "3-ArrayFolder"],
+    }
+    ],
+  server_cluster_definition_array => [
+    {
+      cluster_name     => [
+        "1-ArrayCluster",
+        "2-ArrayCluster"],
+      operating_system => "Novell Suse Linux 10",
+      folder_name      => "1-ArrayFolder",
+    }
+    ,
+    {
+      cluster_name     => "SingleCluster",
+      num_clusters     => 1,
+      operating_system => "Red Hat Linux 5.x",
+      do_not_number    => true,
+      notes            => "Example Notes",
+      alert_on_partial_connectivity => false,
+      alert_on_connectivity         => false,
+    }
+    ,
+    {
+      cluster_name     => "SeriesCluster",
+      num_clusters     => 2,
+      operating_system => "Windows 2012",
+      folder_name      => "SeriesFolder01",
+    }
+    ],
+  server_definition_array         => [
+    {
+      server_name   => "SingleServer",
+      num_servers   => 1,
+      do_not_number => true,
+      alert_on_connectivity         => false,
+      alert_on_partial_connectivity => false,
+      notes         => "Example Notes",
+      wwn_or_iscsi_name             => "YOUR ISCSI NAME",
+      port_type     => "Iscsi",
+    }
+    ,
+    {
+      server_name => "SeriesServer",
+      num_servers => 5,
+      folder_name => "SeriesFolder01",
+    }
+    ,
+    {
+      server_name      => [
+        "1-ArrayServer",
+        "2-ArrayServer"],
+      operating_system => "Novell Suse Linux 10",
+      parent_name      => "1-ArrayCluster",
+    }
+    ,
+    {
+      server_name       => [
+        "ClusterServer"],
+      is_server_cluster => true,
+      operating_system  => "Other Singlepath",
+    }
+    ,
+    ],
+  hba_definition_array            => [{
+      server_name       => "SeriesServer01",
+      port_type         => "Iscsi",
+      wwn_or_iscsi_name => "YOUR OTHER ISCSI NAME",
+    }
+    ],
+}
---
+++ examples/VolumeExamples.pp
@@ -0,0 +1,92 @@
+# This is an example manifest showing how to create Volumes, Volume Folders, and mappings between Volumes and Servers.
+# Fill in the information at the top of the file.
+class { 'dellstorageprovisioning':
+  ip_address => "YOUR DSM IP ADDRESS",
+  password   => "YOUR PASSWORD",
+  username   => "YOUR USERNAME",
+  tear_down  => false, # Change to 'true' to remove set-up
+  default_storage_center         => "YOUR SC ID",
+  server_definition_array        => [
+    {
+      server_name => [
+        "1MappingServer"],
+    }
+    ,
+    {
+      server_name    => [
+        "2MappingServer"],
+    }
+    ],
+  volume_folder_definition_array => [
+    {
+      num_folders   => 1,
+      do_not_number => true,
+      folder_name   => "SingleFolder",
+    }
+    ,
+    {
+      num_folders => 3,
+      folder_name => "SeriesFolder",
+      notes       => "Example Notes",
+      parent_name => "SingleFolder",
+    }
+    ,
+    {
+      folder_name    => [
+        "1-ArrayFolder",
+        "2-ArrayFolder",
+        "3-ArrayFolder"],
+
+    }
+    ],
+  volume_definition_array        => [
+    {
+      volume_name    => "SingleVolume",
+      size           => "500GB",
+      num_volumes    => 1,
+      do_not_number  => true,
+      read_cache     => false,
+      write_cache    => false,
+      data_page_size => "Mb2",
+    }
+    ,
+    {
+      volume_name => "SeriesVolume",
+      num_volumes => 5,
+      size        => "100GB",
+      folder_name => "SeriesFolder01",
+      server_name => "1MappingServer",
+    }
+    ,
+    {
+      volume_name => "SecondSeriesVolume",
+      num_volumes => 10,
+      size        => "1GB",
+    }
+    ,
+    {
+      volume_name    => [
+        "1-ArrayVolume",
+        "2-ArrayVolume",
+        "3-ArrayVolume"],
+      size           => "10GB",
+    }
+    ],
+  mapping_definition_array       => [
+    {
+      volume_name_array => [
+        "1-ArrayVolume",
+        "2-ArrayVolume",
+        "3-ArrayVolume"],
+      server_name       => "2MappingServer",
+    }
+    ,
+    {
+      volume_name_array          => [
+        "SecondSeriesVolume01",
+        "SecondSeriesVolume10"],
+      volume_name_array_is_range => true,
+      server_name                => "1MappingServer",
+    }
+    ],
+}
